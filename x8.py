b8b3db12ed0cf8552f0120058e34a8f2366844af
kevisoft <72778200+kevisoft@users.noreply.github.com>
2021-02-25 03:13:00 -0500

    Create x8.py

---
+++ x8.py
@@ -0,0 +1,33 @@
+import time
+import busio
+import digitalio
+from board import SCK, MOSI, MISO, CE0, CE1
+
+from adafruit_rgb_display import color565
+import adafruit_rgb_display.ili9341 as ili9341
+
+
+# Configuration for CS and DC pins:
+CS_PIN = D2
+DC_PIN = D3
+
+# Setup SPI bus using hardware SPI:
+spi = busio.SPI(clock=SCK, MOSI=MOSI, MISO=MISO)
+
+# Create the ILI9341 display:
+disp = hx8357.HX8357(spi, rotation=180,                 
+disp = hx8357.HX8357(spi, rotation=90,
+                       cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)          
+
+# Main loop:
+while True:
+    # Clear the display
+    display.fill(0)
+    # Draw a red pixel in the center.
+    display.pixel(120, 160, color565(255, 0, 0))
+    # Pause 2 seconds.
+    time.sleep(2)
+    # Clear the screen blue.
+    display.fill(color565(0, 0, 255))
+    # Pause 2 seconds.
+    time.sleep(2)
